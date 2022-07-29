a0e3062afd2ac563c8bb9b135816d5c3c89cf0e8
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-07-29 12:16:54 -0400

    Add files via upload

--- ch_3/ch_3.js
+++ ch_3/ch_3.js
@@ -4,3 +4,44 @@ Chapter 3. Types, Values, and Variables
 Flanagan, David. JavaScript: The Definitive Guide (p. 23). O'Reilly Media. Kindle Edition. 
 */
 
+// a.sort();    // The object-oriented version of sort(a)
+
+// 3.2 Numbers
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 25). O'Reilly Media. Kindle Edition. 
+
+// 3.2.1 Integer Literals
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 26). O'Reilly Media. Kindle Edition. 
+
+0
+3
+10000000
+
+0xff // 255: (15*16 + 15)
+0xBADCAFE   // => 195939070
+
+0b10101 // => (1*16 + 0*8 + 1*4 + 0*2 + 1*1)
+0o377   // => 255: (3*64 + 7*8 + 7*1)
+
+// 3.2.2 Floating-Point Literals
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 26). O'Reilly Media. Kindle Edition. 
+
+3.14
+2345.6789
+.3333333333333333333333
+6.02e23 // 6.02 x 10^23
+1.4738223E-32   // 1.4738223 x 10^-32
+
+// Separators in numeric literals
+
+// let billion = 1_000_000_000; //Underscore as a thousands separator
+// let bytes = 0x89_AB_CD_EF;   // As as bytes separator
+// let bits = 0b0001_1101_0111; // As a nibble separator
+// let fraction = 0.123_456_789;    // Works in the fractional part, too. 
+
+// 3.2.3 Arithmetic in JavaScript
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 27). O'Reilly Media. Kindle Edition. 
+
