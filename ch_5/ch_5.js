08c682e1c8c8a9d37ed0cc7e38e2177023d091f5
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-04 11:04:33 -0400

    Add files via upload

---
+++ ch_5/ch_5.js
@@ -0,0 +1,33 @@
+/*
+
+Chapter 5. Statements
+
+Flanagan, David. JavaScript: The Definitive Guide (p. 97). O'Reilly Media. Kindle Edition. 
+
+*/
+
+// 5.1 Expression Statements
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 98). O'Reilly Media. Kindle Edition. 
+
+// greeting ="Hello" + name;
+i *= 3;
+
+counter++;
+
+delete o.x;
+
+console.log(debugMessage);
+displaySpinner();   // A hypothetical function to display a spinner in a web app. 
+
+// If a function does not have any side effects, there is no sense in calling it,
+// unless it is part of a larger expression or an assignment statement. 
+
+Math.cos(x)     // He says you wouldn't compute this and throw it away
+
+cs = Math.cos(x)    // You'd assign it to a var so you can use it later
+
+// 5.2 Compound and Empty Statements
+
+// Flanagan, David. JavaScript: The Definitive Guide (pp. 98-99). O'Reilly Media. Kindle Edition. 
+
