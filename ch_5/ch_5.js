3e636f6c7eba9cb1846e428642a5765fff2a7728
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-08 14:00:22 -0400

    Add files via upload

--- ch_5/ch_5.js
+++ ch_5/ch_5.js
@@ -452,3 +452,57 @@ factorial(4)    // => 24
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 118). O'Reilly Media. Kindle Edition. 
 
+// try/catch/finally stmt is JavaScript's exception handling mechanism
+
+
+// Ex: illustrating the syntax and purpose of the try/catch/finally stmt
+
+try{
+    // Normally, this code runs from teh top of the block to the bottom
+    // without problems. But it can sometimes throw an exceptioin,
+    // either directly, with a throw statement, or indirectly, by calling
+    // a method that throws an exception
+}
+
+catch(e){
+    // The statements in this block are executed if, and only if, the try
+    // block throws an exception. These statements can use the local variable
+    // e to refer to the Error object or other value that was thrown.
+    // This block may handle the exception somehow, may ignore the 
+    // exception by doing nothing, or may rethrow the exception with throw.
+}
+
+finally{
+    // This block contains statements that are always executed, regardless of
+    // what happens in the try block. They are executed whether the try
+    // block terminates:
+    // 1) normally, after reaching the bottom of the block
+    // 2) because of a break, continue, or return statement
+    // 3) with an exception that is handled by a catch clause above
+    // 4) with an uncaught exception that is still propagating
+}
+
+// Ex: a more realistic example
+
+try{
+    // Ask the user to enter a number
+    let n = Number(prompt("Please enter a positive integer", ""));
+    // Compute the factorial of the number, assuming the input is valid
+    let f = factorial(n);
+    // Display the result
+    alert(n + "!= " + f);
+}
+catch(ex){  // If the user's input was not valid, we end up here
+    alert(ex);  // Tell the user what the error is
+}
+
+// 5.6 Miscellaneous Statements
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 121). O'Reilly Media. Kindle Edition. 
+
+// with, debugger, and "use strict"
+
+// 5.6.1 with
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 121). O'Reilly Media. Kindle Edition. 
+
