6ae6015b530013f28214261ea01ecbd4cec416e3
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-09 20:00:45 -0400

    Add files via upload

---
+++ ch_6/ch_6.js
@@ -0,0 +1,26 @@
+/*
+
+Chapter 6. Objects
+
+Flanagan, David. JavaScript: The Definitive Guide (p. 129). O'Reilly Media. Kindle Edition. 
+
+*/
+
+// 6.1 Introduction to Objects
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 129). O'Reilly Media. Kindle Edition. 
+
+// objects are python dictionaries
+
+// object has an unordered collection of properties, each have a name, value pair
+
+// we say that the objects map string to values 
+
+// 6.2 Creating Objects
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 130). O'Reilly Media. Kindle Edition. 
+
+// 6.2.1 Object Literals
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 131). O'Reilly Media. Kindle Edition. 
+
