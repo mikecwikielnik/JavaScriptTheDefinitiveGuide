47618d79b7cef383e4723df4b661a9836f526e53
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-10 19:20:17 -0400

    Add files via upload

--- ch_6/ch_6.js
+++ ch_6/ch_6.js
@@ -149,3 +149,27 @@ function computeValue(portfolio){
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 135). O'Reilly Media. Kindle Edition. 
 
+let g ={};  // g inherits object methods from Object.prototype
+g.x = 1;    // and it now has an own property x
+let p = Object.create(o);   // p inherits properties from o and Object.prototype
+p.y = 2;    // and has an own property
+let q = Object.create(p);   // q inherits properties from p, g and...
+q.z = 3;    // ... Object.prototype and has an own property z
+let f = q.toString();   // toString is inherited from Object.prototype
+q.x + q.y   // => 3; x and y are inherited from g and p
+
+// ex:
+
+let unitcircle = {r:1};   // An object to inherit from // a mini dictionary
+let c = Object.create(unitcircle);  // c inherits the property r // a JavaScript syntax to create another object (variable)
+c.x = 1; c.y = 1;   // c defines two properties of its own // now the dictionary has 3 names in the name/value pair: r, x, y
+c.r = 2;    // c overrides its inherited property // r is not 2, not 1
+unitcircle.r    // => 1: the prototype is not affected // you only call r through dot notation
+
+
+// see: setter method 6.10.6
+
+// 6.3.3 Property Access Errors
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 137). O'Reilly Media. Kindle Edition. 
+
