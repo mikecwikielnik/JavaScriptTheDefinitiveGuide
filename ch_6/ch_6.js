1bf726c4ed4fc06a6e02a6d29a7b2c5a8d98e1f2
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-09 20:14:04 -0400

    Add files via upload

--- ch_6/ch_6.js
+++ ch_6/ch_6.js
@@ -24,3 +24,20 @@ Flanagan, David. JavaScript: The Definitive Guide (p. 129). O'Reilly Media. Kind
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 131). O'Reilly Media. Kindle Edition. 
 
+let empty = {};     // An object with no properties
+let point = {x:0, y:0};     // Two numeric properties
+let p2 = {x: point.x, y:point.y+1};     // More complex values
+let book = {
+    "main title": "JavaScript",     // These property names include spaces,
+    "sub-title": "The Definitive Guide",    // and hypens, so use string literals.
+    for: "all audiences",   // for is reversed, but no quotes.
+    author: {   // The value of this property is
+        firstname: "David",     // itself an object
+        surname: "Flanagan"
+    }
+};
+
+// 6.2.2 Creating Objects with new
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 131). O'Reilly Media. Kindle Edition. 
+
