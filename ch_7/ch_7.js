f0566bd196319f1d899cf30d755e6cf1fb16cb23
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-13 11:02:00 -0400

    Add files via upload

--- ch_7/ch_7.js
+++ ch_7/ch_7.js
@@ -109,3 +109,35 @@ let truearray = Array.from(arraylike);
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 159). O'Reilly Media. Kindle Edition. 
 
+let a4 = ["world"];     // Start with a one-element array
+let value = a[0];   // Read element 0
+a[1] = 3.14;    // Write element 1
+let i = 2;
+a[i] = 3;   // Write element 2
+a[i + 1] = "hello";     // Write element 3
+a[a[i]] = a[0];     // Read elements 0 and 2, write element 3
+
+a4.length   // => 4
+
+// ex: converting the index from number to a string
+
+let o = {};     // Create a plain object
+o[1] = "one"    // Index it with an integer
+o["1"]  // => "one"; numeric and string property names are the same
+
+// ex: difference between array index and object property name
+
+a[-1.23] = true;    // This creates a property named "-1.23"
+a["1000"] = 0;  // This the 1001st element of the array
+a[1.000] = 1;   // Array index 1. Same as a[1] = 1;
+
+// ex: query of a nonexistent property 
+
+let a = [true, false];  // This array has elements at indexes 0 and 1
+a[2]    // => undefined; no element at this index
+a[-1]   // => undefined; no property with this name
+
+// 7.3 Sparse Arrays
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 160). O'Reilly Media. Kindle Edition. 
+
