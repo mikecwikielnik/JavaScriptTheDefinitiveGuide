8bbc5499c1319faf8d10e370703ce51ea79dedb8
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-13 12:19:21 -0400

    Add files via upload

--- ch_7/ch_7.js
+++ ch_7/ch_7.js
@@ -264,3 +264,24 @@ for(let i=0; i<a.length; i++){
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 164). O'Reilly Media. Kindle Edition. 
 
+// Create a multidimensional array
+let table1 = new Array(10);     // 10 rows of the table
+for(let i=0; i<table.length; i++){
+    table[i] = new Array(10);   // Each row has 10 columns
+}
+
+// Initialize the array
+for(let row=0; row<table.length; row++){
+    for(let col=0; row<table[row].length; col++){
+        table[row][col] = row*col;
+    }
+}
+
+// Use the multidimensional array to compute 5*7
+table[5][7]     // => 35
+
+// 7.8 Array Methods
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 165). O'Reilly Media. Kindle Edition. 
+
+
