b99d894c69719dd6c38eeac91153787a9634e735
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-15 11:43:20 -0400

    Add files via upload

--- ch_7/ch_7.js
+++ ch_7/ch_7.js
@@ -411,3 +411,33 @@ a   // => [1,2,3]; the original array is unmodified
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 170). O'Reilly Media. Kindle Edition. 
 
+let stack = [];     // stack == []
+stack.push(1,2);    // stack == [1,2];
+stack.pop();    // stack == [1]; returns 2
+stack.push(3);  // stack == [1,3]
+stack.pop();    // stack == [1]; returns 3
+stack.push([4,5]);  // stack == [1,[4,5]]
+stack.pop()     // stack == [1]; returns [4,5]
+stack.pop();    // stack == []; returns 1
+
+// ex: push all of the elements from one array onto another
+
+a.push(...value);
+
+let q = [];     // q ==[]
+q.push(1,2);    // q == [1,2]
+q.shift();  // q == [2]; returns 1
+q.push(3)   // q == [2,3]
+q.shift()   // q == [3]; returns 2
+q.shift()   // q == []; returns 3
+
+let a = [];     // a == []
+a.unshift(1)    // a == [1]
+a.unshift(2)    // a == [2,1]
+a = [];     // a == []
+a.unshift(1,2)  // a == [1,2]
+
+// 7.8.5 Subarrays with slice(), splice(), fill(), and copyWithin()
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 172). O'Reilly Media. Kindle Edition. 
+
