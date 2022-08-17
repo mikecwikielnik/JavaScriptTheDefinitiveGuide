4b372733e7b384af4948f986eefc8273dbed8dbc
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-16 20:27:03 -0400

    Add files via upload

--- ch_7/ch_7.js
+++ ch_7/ch_7.js
@@ -586,3 +586,52 @@ Array.isArray({})   // => false
 // 7.9 Array-Like Objects
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 177). O'Reilly Media. Kindle Edition. 
+
+let a = {};     // Start with a regular empty object
+// Add properties to make it "array-like"
+let i1 = o;
+while(i1 < 10){
+    a[i] = i*i;
+    i++;
+}
+a.length = i;
+
+// Now iterate through it as if it were a real array
+let total = 0;
+for(let j=0; j<a.length; j++){
+    total += a[j];
+}
+
+// ex: test for objects that work like arrays:
+
+// Determine if o is an array-like object
+// Strings and functions have numeric length properties, but are
+// excluded by the typeof test. In client-side JavaScript, DOM text
+// nodes have a numeric length property, and may need to be excluded
+// with an additional o.nodeType !== 3 test.
+function isArrayLike(o){
+    if(o &&     // o is not null, undefined, etc.
+        typeof o === "object" &&    // o is an object
+        Number.isFinite(o.length) &&    // o.length is a finite number
+        o.length >= 0 &&    // o.length is non-negative
+        Number.isInteger(o.length) &&   // o.length is an integer
+        o.length < 4294967295){     // o.length < 2^32-1}
+            return true;    // Then o is array-like
+       }else{
+        return false;   // Otherwise it is not
+    }
+}
+
+// ex:
+
+let a = {"0":"a","1":"b","2":"c", length: 3};   // An array-like object
+Array.prototype.join.call(a, "+")   // => "a+b+c"
+Array.prototype.map.call(a, x => x.toUpperCase())   // => ["A","B","C"]
+Array.prototype.slice.call(a, 0)    // => ["a","b","c"]: true array copy
+Array.from(a)   // => ["a","b","c"]: easier array copy
+
+// 7.10 Strings as Arrays
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 179). O'Reilly Media. Kindle Edition. 
+
+
