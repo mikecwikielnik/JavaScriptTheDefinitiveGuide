2fa6833a0e61b2f9a5972c3c6a181b2ea0e60cbf
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-21 11:56:16 -0400

    Add files via upload

--- ch_8/ch_8.js
+++ ch_8/ch_8.js
@@ -339,4 +339,61 @@ timed(benchmark)(1000000)   // => 500000500000; this is the sum of the numbers
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 197). O'Reilly Media. Kindle Edition. 
 
+function vectorAdd(v1,v2){
+    return [v1[0] + v[0], v1[1] + v2[1]];
+}
+vectorAdd([1,2],[3,4])  // => [4,6]
 
+function vectorAdd([x1,y1],[x2,y2]){ // Unpack 2 arguments into 4 parameters
+    return [x1 + x2, y1 + y2];
+}
+vectorAdd([1,2],[3,4])  // => [4,6]
+
+// Multiply the vector {x,y} by a scalar value
+function vectorMultiply({x,y}, scalar){
+    return {x: x*scalar, y: y*scalar};
+}
+vectorMultiply({x: 1, y: 2}, 2)     // => {x: 2, y:4}
+
+function vectorAdd(
+    {x: x1, y: y1},     // Unpack 1st object into x1 and y1 params
+    {x: x2, y: y2}  // Unpack 2nd object into x2 and y2 params
+)
+{
+    return {x: x1 + x2, y: y1 + y2};
+}
+vectorAdd({x: 1, y: 2}, {x: 3, y:  4})  // => {x: 4, y: 6}
+
+// Multiply the vector {x,y} or {x,y,z} by a scalar value
+function vectorMultiply({x,y,z=0}, scalar){
+    return {x: x*scalar, y: y*scalar, z: z*scalar};
+}
+vectorMultiply({x: 1, y: 2}, 2)     // => {x: 2, y: 4, z: 0}
+
+function arraycopy({from, to=from, n=from.length, fromIndex=0,
+toIndex=0}){
+    let valuesToCopy = from.slice(fromIndex, fromIndex + n);
+    to.splice(toIndex,0,...valuesToCopy);
+    return to;
+}
+let a1 = [1,2,3,4,5], b = [9,8,7,6,5];
+arraycopy({from: a, n:3, to:b, toIndex:4})  // => [9,8,7,6,1,2,3,5]
+
+// This function expects an array argument. The first two elements of that
+// array are unpacked into the x and y parameters. Any remaining elements
+// are stored in the coords array. And any arguments after the first array
+// are packed into the rest array.
+function f([x,y,...coords],...rest){
+    return [x+y, ...rest, ...coords];   // Note: spread operator here
+}
+f([1,2,3,4],5,6)    // => [3,5,6,3,4]
+
+// Multiply the vector {x,y} or {x,y,z} by a scalar value, retain other props
+function vectorMultiply({x, y, z=0, ...props}, scalar){
+    return {x: x*scalar, y: y*scalar, z: z*scalar, ...props};
+}
+vectorMultiply({x: 1, y: 2, w: -1}, 2)  // => {x:2, y:4, z:0, w:-1}
+
+function drawCircle({x, y, radius, color:[r,g,b]}){
+    // Not yet implemented
+}
