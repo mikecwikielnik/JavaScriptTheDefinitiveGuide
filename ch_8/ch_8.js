ac059a653454c01b8a3e14906e10645e30c3ae39
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-21 18:04:42 -0400

    Add files via upload

--- ch_8/ch_8.js
+++ ch_8/ch_8.js
@@ -424,3 +424,65 @@ sum([1,2,"3"]);     // !TypeError: element 2 is not a number
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 200). O'Reilly Media. Kindle Edition. 
 
+// ex: consider this function definition
+
+function square(x){return x*x;}
+
+let s = square;     // Now s refers to the same function that squaare does
+square(4)   // => 16
+s(4)    // => 16
+
+// ex: functions "methods"
+
+let o2 = {square: function(x){return x*x;}};     // An object literal
+let y = o.square(16);   // y == 256
+
+// ex: functions don't require names
+
+let a2 = [x => x*x, 20];     // An array literal
+a[0](a[1])  // => 400
+
+// ex: using functions as data
+
+// We define some simple functions here
+function add(x,y){return x+y;}
+function subtract(x,y){return x-y;}
+function multiply(x,y){return x*y;}
+function divide(x,y){return x/y;}
+
+// Here's a function that takes one of the preceding functions
+// as an argument and invokes it on two operands
+function operate(operator, operand1,operand2){
+    return operator(operand1, operand2);
+}
+
+// We could invoke this function like this to compute the value(2+3)+(4*5):
+let i = operate(add, operate(add,2,3), operate(multiply,4,5));
+
+// For the sake of the example, we implement the simple functions again, 
+// this time within an object literal;
+const operators = {
+    add: (x,y) => x+y,
+    subtract: (x,y) => x-y,
+    multiply: (x,y) => x*y,
+    divide: (x,y) => x/y,
+    pow: Math.pow   // This works for predefined functions too
+};
+
+// This function takes the name of an operator, looks up that operator
+// in the object, and then invokes it on the supplied operands. Note
+// the syntax used to invoke the operator function.
+function operate2(operation, operand1, operand2){
+    if(typeof operators[operation] === "function"){
+        return operators[operation](operand1,operand2);
+    }
+    else throw "unknown operator";
+}
+
+operate2("add","hello",operate2("add","","world"))  // => "hello world"
+operate2("pow",10,2)    // => 100
+
+// 8.4.1 Defining Your Own Function Properties
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 202). O'Reilly Media. Kindle Edition. 
+
