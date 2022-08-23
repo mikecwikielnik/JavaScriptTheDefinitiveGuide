7878863fcb4b5fbd8ef03fe43681e9c69765467e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-22 22:45:25 -0400

    Add files via upload

--- ch_8/ch_8.js
+++ ch_8/ch_8.js
@@ -767,3 +767,63 @@ constructFunction()()   // => "global"
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 213). O'Reilly Media. Kindle Edition. 
 
+// ex: the following sections are for mind-expanding exploration of the power of JavaScript
+// not a prescription for a good programming style
+
+// 8.8.1 Processing Arrays with Functions
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 214). O'Reilly Media. Kindle Edition. 
+
+// ex: compute the mean and standard deviation of an array
+
+let data = [1,2,3,5,5];     // This is our array of numbers
+
+// The mean is the sum of the elements divided by the number of elements
+let total1 = 0;
+for(let i=0; i<data.length; i++)total += data[i];
+let mean = total/data.length;   // mean == 3; the mean of our data is 3
+
+// To compute the standard deviation, we first sum the squares of
+// the deviation of each element from the mean
+total = 0;
+for(let i=0; i<data.length; i++){
+    let deviation = data[i] - mean;
+    total += deviation * deviation;
+}
+let stddev = Math.sqrt(total/(data.length-1));  // stddev == 2
+
+// ex: array methods map() and reduce()
+
+// First, define two simple functions
+const sum = (x,y) => x+y;
+const square = x => x*x;
+
+// Then use those functions with Array methods to compute mean and stddev
+let data1 = [1,1,3,5,5];
+let mean1 = data.reduce(sum)/data.length;   // mean == 3
+let deviations = data.map(x=>x-mean);
+let stddev1 = Math.sqrt(deviations.map(square).reduce(sum)/(data.length-1));
+stddev1     // => 2
+
+// ex: Let's write functional versions of the map() and reduce() methods:
+
+const map = function(a, ...args){return a.map(...args);};
+const reduce = function(a, ...args){return a.reduce(...args);};
+
+// With these map() and reduce() functions defined, our code to compute
+// mean and standard deviation now looks like this:
+
+const sum = (x,y) => x+y;
+const square = x => x*x;
+
+let data2 = [1,1,3,5,5];
+let mean2 = reduce(data, sum)/data.length;
+let deviations1 = map(data, x=>x-mean);
+let stddev2 = Math.sqrt(reduce(map(deviations1,square), sum)/(data.length-1));
+stddev2     // => 2
+
+// 8.8.2 Higher-Order Functions
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 215). O'Reilly Media. Kindle Edition. 
+
+
