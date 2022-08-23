d0cf7b953ce3dbdb29475bca7dbcde8a75ef9963
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-23 12:20:29 -0400

    Add files via upload

--- ch_9/ch_9.js
+++ ch_9/ch_9.js
@@ -55,3 +55,42 @@ r.toString()    // => "(1...3"
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 224). O'Reilly Media. Kindle Edition. 
 
+// ex: 9-2 A Range class using a constructor
+// this is older JavaScript code. For reference
+
+// This is a constructor function that initializes new Range objects.
+// Note that it does not create or return the object. It just initializes this.
+function Range(from, to){
+    // Store the start and end points (state) of this new range object.
+    // These are noninherited properties that are unique to this object. 
+    this.from = from;
+    this.to = to;
+}
+
+// All range objects inherit from this object
+// Note that the property name must be "prototype" for this to work
+Range.prototype = {
+    // Return true if x is in the range, false otherwise
+    // This method works for textual and Date ranges as well as numeric
+    includes: function(x){return this.from <= x && x <= this.to;},
+
+    // A generator function that makes instances of the class iterable.
+    // Note that it only works for numeric ranges
+    [Symbol.iterator]: function*(){
+        for(let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+    },
+
+    // Return a string representation of the range
+    toString: function(){return "(" + this.from + "..." + this.to + ")";}
+};
+
+// Here are example uses of this new Range class
+let r1 = new Range(1,3);     // Create a Range object; note the use of new
+r1.includes(2)  // => true: 2 is in the range
+r1.toString()   // => "(1...3)"
+// [...r1]     // => [1,2,3]; convert an array via iterator
+
+// 9.2.1 Constructors, Class Identity, and instanceof
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 226). O'Reilly Media. Kindle Edition. 
+
