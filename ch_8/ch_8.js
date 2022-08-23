347ac6633637d93cd800dfe1e83ad42e05065d81
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-23 10:14:59 -0400

    Add files via upload

--- ch_8/ch_8.js
+++ ch_8/ch_8.js
@@ -918,3 +918,57 @@ cuberoot(increment(26))     // => 3
 // 8.8.4 Memoization
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 217). O'Reilly Media. Kindle Edition. 
+
+// Return a memoized version of f
+// It only works if arguments to f all have distinct string representations
+
+function memoize(f){
+    const cache = new Map();    // Value cache stored in the closure. 
+
+    return function(...args){
+        // Create a string version of the arguments to use as a cache key
+        let key = args.length + args.join("+");
+        if(cache.has(key)){
+            return cache.get(key)
+        }else{
+            let result = f.apply(this, args);
+            cache.set(key, result);
+        }
+    };
+}
+
+// Return the Greatest Common Divisor of two integers using the Euclidian
+// algorithm: http://en.wikipedia.org/wiki/Euclidean_algorithm
+function gcd(a,b){ // Type checking for a and b has been omitted
+    if(a<b){ // Ensure that a>=b when we start
+        [a,b] = [b,a];  // Destructing assignment to swap variables
+    }
+    while(b!==0){ // This is Euclid's algorithm for GCD
+        [a,b] = [b,a%b];
+    }
+    return a;
+}
+
+const gcdmemo = memoize(gcd);
+gcdmemo(85,187)     // => 17
+
+// Note that when we write a recursive function that we will be memoizing,
+// we typically want to recurse to the memoized version, not the original.
+const factorial = memoize(function(n){
+    return (n<=1) ? 1:n*factorial(n-1);
+});
+factorial(5)    // => 120: also caches values for 4,3,2, and 1.
+
+// 8.9 Summary
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 220). O'Reilly Media. Kindle Edition. 
+
+// You can define functions wiht the function keyword and with => syntax
+
+// You can use the spread operator to pass elements of an array or iterable object
+// as arguments in a function invocation
+
+// functions called closures, are defined inside of and returned by an enclosing
+// function retains access to its lexical scope. This means it can read/write
+// the variable defined inside the outer function 
+
