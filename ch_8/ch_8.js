691390aacee396cd298864a224f78b5af7f6668f
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-22 23:15:28 -0400

    Add files via upload

--- ch_8/ch_8.js
+++ ch_8/ch_8.js
@@ -877,3 +877,44 @@ compose(square, sum)(2,3)   // => 25; the square of the sum
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 216). O'Reilly Media. Kindle Edition. 
 
+// The arguments to this function are passed on the left
+function partialLeft(f, ...outerArgs){
+    return function(...innerArgs){ // Return this function
+        let args = [...outerArgs, ...innerArgs];    // Build the argument list
+        return f.apply(this, args);     // Then invoke f with it
+    };
+}
+
+// The arugments to this function serve as a template. Undefined values
+// in the argument list are filled in with values from the inner set.
+function partial(f, ...outerArgs){
+    return function(...innerArgs){
+        let args = [...outerArgs];  // local copy of outer args template
+        let innerIndex = 0;     // which inner arg is next
+        // Loop through the args, filling in undefined values from inner args
+        for(let i=0; i<args.length; i++){
+            if(args[i] === undefined) args[i] = innerArgs[innerIndex++];
+        }
+        // Now append any remaining inner arguments
+        args.push(...innerArgs.slice(innerIndex));
+        return f.apply(this, args);
+    };
+}
+
+// Here is a function with three arguments
+const f = function(x,y,z){return x*(y-z)};
+// Notice how these three partial applications differ
+partialLeft(f,2)(3,4)   // => -2: Bind first argument: 2 * (3-4)
+partialRight(f,2)(3,4)  // => 6: Bind last argument: 3 * (4-2)
+partial(f, undefined, 2)(3,4)   // => -6: Bind middle argument: 3 * (2-4)
+
+
+// ex:
+
+const increment = partialLeft(sum, 1);
+const cuberoot = partialRight(Math.pow, 1/3);
+cuberoot(increment(26))     // => 3
+
+// 8.8.4 Memoization
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 217). O'Reilly Media. Kindle Edition. 
