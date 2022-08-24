43c4169c93540a30de214d087fde740897b88422
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-23 20:14:59 -0400

    Add files via upload

--- ch_9/ch_9.js
+++ ch_9/ch_9.js
@@ -110,3 +110,34 @@ range.methods.isPrototypeOf(r);     // range.methods is the prototype object.
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 228). O'Reilly Media. Kindle Edition. 
 
+let F = function(){};   // This is a function object.
+let p = F.prototype;    // This is the prototype object associated with F.
+let c = p.constructor;  // This is the function associated with the prototype
+c === F     // => true: F.prototype.constructor === F for any F
+
+let o = new F();    // Create an object o of class F
+o.constructor === F     // => true: the constructor property specifies the class
+
+// ex:
+
+Range.prototype = {
+    constructor: Range,     // Explicitly set the constructor back-reference
+
+    /* method definitions go here */
+};
+
+// ex: another older JavaScript technique
+
+// Extend the predefined Range.prototype object so we don't overwrite
+// the automatically created Range.prototype.constructor property
+Range.prototype.includes = function(x){
+    return this.from <= x && x <= this.to;
+};
+Range.prototype.toString = function(){
+    return "(" + this.from + "..." + this.to + ")";
+};
+
+// 9.3 Classes with the class Keyword
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 229). O'Reilly Media. Kindle Edition. 
+
