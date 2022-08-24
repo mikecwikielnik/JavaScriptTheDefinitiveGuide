6615c7a5df61128890ffbdacdcc50f545b355154
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-24 07:50:30 -0400

    Add files via upload

--- ch_9/ch_9.js
+++ ch_9/ch_9.js
@@ -230,3 +230,46 @@ r3.parse('(1...10)');   // TypeError: r.parse is not a function
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 232). O'Reilly Media. Kindle Edition. 
 
+// ex: constructor that initializes three fields
+
+class Buffer {
+    constructor(){
+        this.size = 0;
+        this.capacity = 4096;
+        this.buffer = new Uint8Array(this.capacity);
+    }
+}
+
+// ex: With the new instance field syntax likely to be standardized, you could write instead:
+
+class Buffer { // More like python
+    size = 0;
+    capacity = 4096;
+    buffer = new Uint8Array(this.capacity);
+}
+
+// ex: # makes fields private, which means immutable
+
+class Buffer {
+    #size = 0;
+    get size(){return this.#size;}
+}
+
+// ex: static field
+
+/*
+
+static integerRangePattern = /^\((\d+)\.\.\.(\d+)\)$/;
+static parse(s){
+    let matches = s.match(Range.integerRangePattern);
+    if(!matches){
+        throw new TypeError(`Cannot parse Range from "${s}".`)
+    }
+    return new Range(parseInt(matches[1]), matches[2]);
+}
+
+*/
+
+// 9.3.4 Example: A Complex Number Class
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 234). O'Reilly Media. Kindle Edition. 
