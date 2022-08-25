831fe886fc6140a6feaf5277b66ace0470ff571f
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-25 12:17:44 -0400

    Add files via upload

--- ch_10/ch_10.js
+++ ch_10/ch_10.js
@@ -6,3 +6,60 @@ Flanagan, David. JavaScript: The Definitive Guide (p. 249). O'Reilly Media. Kind
 
 */
 
+// 10.1 Modules with Classes, Objects, and Closures
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 250). O'Reilly Media. Kindle Edition. 
+
+// ex:
+
+const BitSet = (function(){ // Set BitSet to the return value of this function
+    // Private implementation details here
+    // function isValid(set, n){...}
+    // function has(set, byte, bit){...}
+    const BITS = new Uint8Array([1,2,4,8,16,32,64,128]);
+    const MASKS = new Uint8Array([~1,~2,~4,~8,~16,~32,~64,~128]);
+
+    // The public API of the module is just the BitSet class, which we define
+    // and return here. The class can use the private functions and constants
+    // defined above, but they will be hidden from users of the class
+    return class BitSet extends AbstractWritableSet{
+        /// ... implementation omitted
+    };
+}());
+
+// The following code, for example, defines a mini statistics module 
+
+// that exports mean() and stddev() functions while leaving the implementation details hidden:
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 251). O'Reilly Media. Kindle Edition. 
+
+// This is how we could define a stats module
+const stats = (function(){
+    // Utility functions private to the module
+    const sum = (x, y) => x + y;
+    const square = x => x * x;
+
+    // A public function that will be exported 
+    function mean(data){
+        return data.reduce(sum)/data.length;
+    }
+
+    // A public function that we will export
+    function stddev(data){
+        let me = mean(data);
+        return Math.sqrt(
+            data.map(x => x - m).map(square).reduce(sum)/(data.length - 1)
+        );
+    }
+
+    // We export the public function as properties of an object
+    return{mean, stddev};
+}());
+
+// And here is how we mmight use the module
+stats.mean([1,3,5,7,9])     // => 5
+stats.stddev([1,3,5,7,9])   // => Math.sqrt(10)
+
+// 10.1.1 Automating Closure-Based Modularity
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 251). O'Reilly Media. Kindle Edition. 
