3ef2370a530d1b7eb850af5f08bebe9f04419d85
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-30 13:38:21 -0400

    Add files via upload

--- ch_12/ch_12.js
+++ ch_12/ch_12.js
@@ -242,3 +242,66 @@ let o = {
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 334). O'Reilly Media. Kindle Edition. 
 
+// ex: generator function that yields Fibonacci numbers:
+
+function* fibonacciSequence(){
+    let x = 0, y = 0;
+    for(;;){
+        yield y;
+        [x, y] = [y, x + y];    // Note: 
+    }
+}
+
+// ex:
+
+// Return the nth Fibonacci number
+function fibonacci(n){
+    for(let f of fibonacciSequence()){
+        if(n-- <= 0) return f;
+    }
+}
+fibonacci(20)   // => 10946
+
+// ex: infinite generatro becomes more useful with a take() generator:
+
+// Yield the first n elements of the specified iterable object
+function* take(n, iterable){
+    let i = iterable[Symbol.iterator]();    // Get iterator for iterable object
+    while(n-- > 0){     // Loop n times:
+        let next = it.next();   // Get the next items from the iterator.
+        if(next.done) return;   // If there are no more values, return early
+        else yield next.value;  // otherwise, yield the value
+    }
+}
+
+// An array of the first 5 Fibonacci numbers
+// [...take(5, fibonacciSequence())]    // => [1,1,2,3,5]
+
+// ex: generator function that interleaves the elements of multiple iterable objects
+
+// Given an array of iterables, yield their elements in interleaved order
+function* zip(...iterables){
+    // Get an iterator for each iterable
+    let iterators = iterables.map(i => i[Symbol.iterator]());
+    let index = 0;
+    while(iterators.length > 0){     // While there are still some iterators
+        if(index >= iterators.length){  // If we reached the last iterator
+            index = 0;  // go back to the first one
+        }
+        let item = iterators[index].next();     // Get next item from next iterator.
+        if(item.done){  // If that iterator is done
+            iterators.splice(index, 1);     // then remove it from the array
+        }
+        else{   // Otherwise,
+            yield item.value;   // yield the iterated value
+            index++;    // and move on to the next iterator
+        }
+    }
+}
+
+// Interleave three iterable objects
+// [...zip(oneDigitPrimes(), "ab",[0])]     // => [2,"a",0,3,"b",5,7]
+
+// 12.3.2 yield* and Recursive Generators
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 335). O'Reilly Media. Kindle Edition. 
