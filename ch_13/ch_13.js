5d075e687ab8d1aed83f3545967c78e521beba5e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-08-31 14:11:08 -0400

    Add files via upload

--- ch_13/ch_13.js
+++ ch_13/ch_13.js
@@ -77,3 +77,65 @@ function getCurrentVersionNumber(versionCallback){  // Note callback argument
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 345). O'Reilly Media. Kindle Edition. 
 
+const fs = require("Fs");   // The "fs" module has filesystem-related APIs
+const { request } = require("http");
+let options = {     // An object to hold options for our program
+    // default options would go here
+};
+
+// Read a configuration file, then call the callback function
+fs.readFile("config.json", "utf-8", (err, text) => {
+    if(err){
+        // If there was an error, display a warning, but continue
+        console.warn("Could not read config file:", err);
+    }else{
+        // Otherwise, parse the file contents and assign to the options object
+        Object.assign(options, JSON.parse(text));
+    }
+
+    // In either case, we can now start running the program
+    startProgram(options);
+});
+
+// ex:
+
+const https = require("https");
+
+// Read teh text content of the URL and asynchronously pass it to the callback.
+function getText(url, callback){
+    // Start an HTTP GET request for the URL
+    request = https.get(url);
+
+    // Register a function to handle the "response" event.
+    request.on("response", response => {
+        // The response event means that response headers have been received
+        let httpStatus = response.statusCode;
+
+        // The body of the HTTP response has not been received yet.
+        // So we register more event handlers to to be called when it arrives
+        response.setEncoding("utf-8");  // We're expecting Unicode text
+        let body = "";  // which we will accumulate here
+
+        // This event handler is called when a chunk of the body is ready
+        response.on("data", chunk => {body += chunk;});
+
+        // This event handler is called when the resposne is complete
+        response.on("end", () => {
+            if(httpStatus === 200){ // IF the HTTP response was good
+                callback(null, body);   // Pass response body to the callback
+            }else{  // Otherwise pass an error
+                callback(httpStatus, null);
+            }
+        });
+    });
+
+    // We also register an event handler for lower-level network errors
+    request.on("error", (err) => {
+        callback(err, null);
+    });
+}
+
+// 13.2 Promises
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 346). O'Reilly Media. Kindle Edition. 
+
