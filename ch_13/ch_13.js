0df2170b0eb906881b4871420bd53df640d69256
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-09-02 11:47:39 -0400

    Add files via upload

--- ch_13/ch_13.js
+++ ch_13/ch_13.js
@@ -688,3 +688,127 @@ async function test(){  // Async so we can use for/await
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 373). O'Reilly Media. Kindle Edition. 
 
+function clock(interval, max = Infinity){
+    // A Promise-ified version of setTimeout that we can use awaith with.
+    // Note that this takes an absolute time instead of an interval
+    function until(time){
+        return new Promise(resolve => setTimeout(resolve, time - Date.now()));
+    }
+
+    // Return an asynchronously iterable object
+    return {
+        startTime: Date.now(),  // Remember when we started
+        count: 1,   // Remember which iteration we're on
+        async next(){   // The next() method makes this an iterator
+            if(this.count >max){    // Are we done?
+                return { done: true};   // Iteration result indicating done
+            }
+            // Figure out when the next iteration should begin, 
+            let targetTime = this.startTime + this.count * interval;
+            // wait until that time,
+            await until(targetTime);
+            // and return the count value in an iteration result object
+            return {value: this.count++ };
+        },
+        // This method means that this iterator object is also an iterable.
+        [Symbol.asyncIterator](){ return this;}
+    };
+}
+
+/**
+ * An asynchronously iterable queue class. Add values with enqueue()
+ * and remove them dequeue(). dequeue() returns a Promise, which
+ * means that values can be dequeued before tehy are enqueued. The
+ * class implements [Symbol.asyncIterator] and next() so that it can
+ * be used with the for/await loop (which will not terminate until 
+ * the close() method is called. )
+ */
+
+class AsyncQueue{
+    constructor(){
+        // Values that have been queued but not dequeued yet are stored here
+        this.values = [];
+        // When Promises are dequeued before their corresponding values are
+        // queued, the resolve methods for those Promises are stored here.
+        this.resolvers = [];
+        // Once closed, no more values can enqueued, and no more unfulfilled
+        // Promises returned.
+        this.closed = false;
+    }
+
+    enqueue(value){
+        if(this.close){
+            throw new Error("AsyncQueue closed");
+        }
+        if(this.resolvers.length > 0){
+            // If this value has already been promised, resolve that Promise
+            const resolve = this.resolvers.shift();
+            resolve(value);
+        }
+        else{
+            // Otherwise, queue it up
+            this.values.push(value);
+        }
+    }
+
+    dequeue(){
+        if(this.values.length > 0){
+            // If there is a queued value, return a resolved Promise for it
+            const value = this.values.shift();
+            return Promise.resolve(value);
+        }
+        else if(this.closed){
+            // If no queued values and we're closed, return a resolved
+            // Promise for the "end-of-stream" marker
+            return Promise.resolve(AsyncQueue.EOS);
+        }
+        else{
+            // Otherwise, return an unresolved Promise,
+            // queuing the resolver function for later use
+            return new Promise((resolve) => {this.resolvers.push(resolve);});
+        }
+    }
+
+    close(){
+        // Once the queue is closed, no more values will be enqueued.
+        // So resolve any pending Promises with the end-of-stream marker
+        while(this.resolvers.length > 0){
+            this.resolvers.shift()(AsyncQueue.EOS);
+        }
+        this.closed = true;
+    }
+
+    // Define the method that makes this class asynchronously iterable
+    [Symbol.asyncIterator](){ return this;}
+
+    // Define the method that makes this an asynchronously iterator. The
+    // dequeue() Promise resolves to a value or the EOS sentinel if we're
+    // closed. Here, we need to return a Promise that resolves to an 
+    // iterator result object.
+    next(){
+        return this.dequeue().then(value => (value === AsyncQueue.EOS)
+            ?{ value: undefined, done: true }
+            :{ value: value, done: false });
+    }
+}
+
+// A sentinel value returned by dequeue() to mark "end of stream" when closed
+AsyncQueue.EOS = Symbol("end of stream");
+
+// ex: 
+
+// Push events of the specified type on the specified document element
+// onto an AsyncQueue object, and return the queue for use as an event stream
+function eventStream(elt, type){
+    const q = new AsyncQueue();     // Create a queue
+    elt.addEventListener(type, e => q.enqueue(e));  // Enqueue events
+    return q
+}
+
+async function handleKeys(){
+    // Get a stream of keypress events and loop once for each one
+    for await (const event of eventStream(document, "keypress")){
+        console.log(event.key);
+    }
+}
+
