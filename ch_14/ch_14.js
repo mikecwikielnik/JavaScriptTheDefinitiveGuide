8ab44154fbc25f70da1cb835fc50fdb38fd4ad4c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-09-06 18:45:22 -0400

    Add files via upload

--- ch_14/ch_14.js
+++ ch_14/ch_14.js
@@ -301,3 +301,43 @@ let a1 = new NonSpreadableArray(1,2,3);
 // 14.4.6 Pattern-Matching Symbols
 
 // Flanagan, David. JavaScript: The Definitive Guide (p. 392). O'Reilly Media. Kindle Edition. 
+
+// ex:
+
+class Glob {
+    constructor(glob){
+        this.glob = glob;
+
+        // We implement glob matching using RegExp Internally
+        // ? matches any one character except /, and * matches zero or more
+        // of those characters. We use capturing groups around each.
+        let regexpText = glob.replace("?", "([^/]").replace("*","([^/]*)");
+
+        // We use the u flag to get Unicode-aware matching.
+        // Globs are intended to match entire strings, so we use the ^ and $
+        // anchors and do not implement search() or matchAll() since they 
+        // are not useful with patterns like this.
+        this.regexp = new RegExp(`^${regexpText}$`, "u");
+    }
+
+    toString(){ return this.glob; }
+
+    [Symbol.search](s){ return s.search(this.regexp); }
+    [Symbol.match](s){ return s.match(this.regexp); }
+    [Symbol.replace](s, replacement){
+        return s.replace(this.regexp, replacement);
+    }
+}
+
+let pattern = new Glob("docs/*.txt");
+"docs/js.txt".search(pattern)   // => 0: matcches at character 0
+"docs/js.htm".search(pattern)   // => -1: does not match
+let match = "docs/js.txt".match(pattern);
+match[0]    // => "docs/js.txt"
+match[1]    // => "js"
+match.index     // => 0
+"docs/js.txt".replace(pattern, "web/$1.htm")    // => "web/js.htm"
+
+// 14.4.7 Symbol.toPrimitive
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 394). O'Reilly Media. Kindle Edition. 
