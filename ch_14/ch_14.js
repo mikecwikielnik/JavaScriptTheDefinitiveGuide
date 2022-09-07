29147a703826a61c00fc495013ff63d25d2fb6ca
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-09-07 12:41:54 -0400

    Add files via upload

--- ch_14/ch_14.js
+++ ch_14/ch_14.js
@@ -338,6 +338,49 @@ match[1]    // => "js"
 match.index     // => 0
 "docs/js.txt".replace(pattern, "web/$1.htm")    // => "web/js.htm"
 
-// 14.4.7 Symbol.toPrimitive
+// 14.5 Template Tags
 
-// Flanagan, David. JavaScript: The Definitive Guide (p. 394). O'Reilly Media. Kindle Edition. 
+// Flanagan, David. JavaScript: The Definitive Guide (p. 395). O'Reilly Media. Kindle Edition. 
+
+// ex:
+
+function html(strings, ...values){
+    // Convert each value to a string and escape special HTML characters
+    let escaped = values.map(v => String(v)
+                .replace("&","&amp;")
+                .replace("<", "&lt;")
+                .replace(">","&gt;")
+                .replace("","&quot;")
+                .replace("","&#39;"));
+
+    // Return the concatenated strings and escaped values
+    let result = strings[0];
+    for(let i = 0; i < escaped.length; i++){
+        result += escaped[i] + strings[i+1];
+    }
+    return result;
+}
+
+let operator = "<";
+html`<b>x${operator}y</b>`  // => "<b>x&lt;y</b>"
+
+// ex:
+
+function glob(strings, ...values){
+    // Assemble the strings and values into a single string
+    let s = strings[0];
+    for(let i = 0; i < values.length; i++){
+        s += values[i] + strings[i + 1];
+    }
+    // Return a parsed representation of that string
+    return new Glob(s);
+}
+
+let root = "/tmp";
+let filePattern = glob`${root}/*.html`;     // A RegExp alternative
+"/tmp/test.html".match(filePattern)[1]  // => "test"
+
+// 14.6 The Reflect API
+
+// Flanagan, David. JavaScript: The Definitive Guide (p. 397). O'Reilly Media. Kindle Edition. 
+
